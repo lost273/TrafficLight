88df13cfb992e22dc31cd2b7b1241e1cbba0e9c5
lost273 <dnk273@gmail.com>
2018-01-03 16:27:28 +0300

    03-01-2018

--- src/tfpackage/TrafficLightSimulator.java
+++ src/tfpackage/TrafficLightSimulator.java
@@ -45,4 +45,18 @@ public class TrafficLightSimulator implements Runnable{
             changeColor();
         }
     }
+    synchronized void changeColor(){
+        switch(tlc){
+            case RED:
+                tlc = TrafficLightColor.GREEN;
+                break;
+            case YELLOW:
+                tlc = TrafficLightColor.RED;
+                break;
+            case GREEN:
+                tlc = TrafficLightColor.YELLOW;
+        }
+        changed = true;
+        notify();
+    }
 }
